21e87470493fdcc23d3c8b701f47b525c6bfb8c2
thomas <a8484149@gmail.com>
2018-10-31 17:23:14 +0900

    crong의 코드리뷰를 반영한 리팩토링 완료
    변수명을  알맞게 변경함

--- lecture3/L3_S1.js
+++ lecture3/L3_S1.js
@@ -4,22 +4,22 @@ class todoProgram {
 
   // 변수 생성하기
   constructor() {
-    this.task = [];
+    this.taskList = [];
     this.id = 1;
   }
 
   // 한줄띄어쓰는 함수
-  newline() {
+  newLine() {
     console.log(``);
   }
 
   // 현재상태를 출력하는 함수
-  printNow() {
-    this.doneCount = 0;
+  printStatus() {
+    this.doneCount = 0; 
     this.doingCount = 0;
     this.todoCount = 0;
-    for (let keys of this.task) {
-      const { status } = keys;
+    for (let task of this.taskList) {
+      const { status } = task;
       if (status === 'todo') this.todoCount++;
       if (status === 'doing') this.doingCount++;
       if (status === 'done') this.doneCount++;
@@ -34,16 +34,16 @@ class todoProgram {
 
   // 새로운 항목을 추가하는 함수
   add({ name, tag }) {
-    this.task.push({
+    this.taskList.push({
       name: name,
       tag: tag,
       id: this.id,
       status: 'todo'
     })
     this.printUpdate(this.id, name, '항목이 새로 추가됐습니다.');
-    this.printNow();
+    this.printStatus();
     this.id++;
-    this.newline();
+    this.newLine();
   }
 
   // 현재상태를 갱신 하는 함수
@@ -52,39 +52,39 @@ class todoProgram {
     let newStatus = nowStatus.nextstatus.toLowerCase();
     let name;
 
-    for (let keys of this.task) {
-      if (keys.id === nowStatus.id) {
-        name = keys.name;
-        oldStatus = keys.status;
-        keys.status = newStatus;
+    for (let task of this.taskList) {
+      if (task.id === nowStatus.id) {
+        name = task.name;
+        oldStatus = task.status;
+        task.status = newStatus;
       }
     }
     this.printUpdate(nowStatus.id, name, `항목이 ${oldStatus} => ${newStatus} 상태로 업데이트 됐습니다.`);
-    this.printNow();
-    this.newline();
+    this.printStatus();
+    this.newLine();
   }
 
   // 기존 항목을 삭제하는 함수
   remove(existing) {
     let existingLecture;
-    for (let keys of this.task) {
-      if (keys.id === existing.id) {
-        existingLecture = keys;
-        this.task.splice(this.task.indexOf(keys), 1);
+    for (let task of this.taskList) {
+      if (task.id === existing.id) {
+        existingLecture = task;
+        this.taskList.splice(this.taskList.indexOf(task), 1);
       }
     }
     this.printUpdate(existingLecture.id, existingLecture.name, '삭제완료.');
-    this.printNow();
-    this.newline();
+    this.printStatus();
+    this.newLine();
   }
 }
 
 const todo = new todoProgram();
-// todo.add({name: "쉬기", tag:"rest"});
-// todo.update({id:1,  nextstatus:"dOing"});
-// todo.update({id:1,  nextstatus:"Done"});
-// todo.add({name: "놀기", tag:"rest"});
-// todo.update({id:2,  nextstatus:"dOing"});
-// todo.update({id:2,  nextstatus:"Done"});
-// todo.remove({id:1});
-// todo.remove({id:2});
+todo.add({name: "쉬기", tag:"rest"});
+todo.update({id:1,  nextstatus:"dOing"});
+todo.update({id:1,  nextstatus:"Done"});
+todo.add({name: "놀기", tag:"rest"});
+todo.update({id:2,  nextstatus:"dOing"});
+todo.update({id:2,  nextstatus:"Done"});
+todo.remove({id:1});
+todo.remove({id:2});
